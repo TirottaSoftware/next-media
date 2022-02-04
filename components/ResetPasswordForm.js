dcc81963fc5c01914c2b8c07b7cb0c8d4ae3526c
Cristian <i449915@student.fontys.nl>
2022-02-04 15:52:29 +0200

    Restyled ResetPasswordForm

--- components/ResetPasswordForm.js
+++ components/ResetPasswordForm.js
@@ -14,13 +14,13 @@ function ResetPasswordForm(props) {
   return (
     <div className="flex flex-col">
       <form
-        className="flex bg-blue-100 flex-col items-center border border-black w-2/5 p-5 m-auto my-5"
+        className="flex bg-white flex-col items-center rounded-2xl w-4/5 lg:w-2/5 p-5 m-auto my-5"
         onSubmit={handleSubmit}
       >
         <h1 className="text-2xl mb-3">Choose a new password</h1>
         <p className="text-center text-red-500">{props.errorMessage}</p>
         <input
-          className="border w-full border-black p-2 m-3"
+          className="w-full outline-none shadow-md border shadow-gray-400 rounded-lg py-3 p-2 m-3 mt-8"
           onChange={(e) => {
             setPassword(e.target.value);
           }}
@@ -29,7 +29,7 @@ function ResetPasswordForm(props) {
           placeholder="New Password"
         />
         <input
-          className="border w-full border-black p-2 m-3"
+          className="w-full outline-none shadow-md border shadow-gray-400 rounded-lg py-3 p-2 m-3 "
           onChange={(e) => {
             setPasswordConfirm(e.target.value);
           }}
@@ -38,7 +38,7 @@ function ResetPasswordForm(props) {
           placeholder="Repeat New Password"
         />
         <input
-          className="cursor-pointer p-2 m-2 bg-blue-400 w-full text-white"
+          className="cursor-pointer p-2 py-3 text-xl rounded-xl m-2 bg-blue-400 w-full text-white"
           type="submit"
         />
       </form>
